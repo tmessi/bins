bb159b8df420517d1a4d016490c2645fe705f10c
Timothy Messier <tim.messier@gmail.com>
2018-09-14 08:49:22 -0400

    screenshot: Add option to copy image directly to the clipboard

--- screenshot.sh
+++ screenshot.sh
@@ -7,17 +7,19 @@ function print_help() {
 
 mode:
     full          capture the full screen
-    window        capture the window that is clicked on
+    window        capture the window that is clicked or an area if you click and drag
 
 optional args:
+    -c|--clipboard place the image on the clipboard instead of a file
     -h|--help     print this help."
 }
 
-OPTS=$(getopt -o h --long help -n "$name" -- "$@")
+OPTS=$(getopt -o h,c --long help,clipboard -n "$name" -- "$@")
 
 # create dir for screenshots
 mkdir -p ~/screenshots
 scrot_opt="-m"
+clipboard=0
 
 if [[ $? != 0 ]]; then echo "option error" >&2; exit 1; fi
 
@@ -29,6 +31,10 @@ while true; do
             print_help
             exit 0
             ;;
+        -c|--clipboard)
+            clipboard=1
+            shift
+            ;;
         --)
             shift; break;;
         *)
@@ -37,22 +43,27 @@ while true; do
 done
 
 if [[ $# -eq 1 ]]; then
-	case $1 in
-	full)
+    case $1 in
+    full)
         scrot_opt="-m"
-		;;
-	window)
-		sleep 1
+        ;;
+    window)
+        sleep 1
         scrot_opt="-s"
-		;;
-	*)
+        ;;
+    *)
         echo "Invalid option"
         print_help
         exit -1
-		;;
-	esac;
+        ;;
+    esac;
 
-    scrot $scrot_opt '%Y-%m-%d-%H%M%S_$wx$h.png' -e 'mv $f ~/screenshots'
+    if [[ $clipboard -eq 1 ]]; then
+        scrot $scrot_opt /tmp/screenshot.png && xclip -selection clipboard -t image/png -i /tmp/screenshot.png
+        rm -f /tmp/screenshot.png
+    else
+        scrot $scrot_opt '%Y-%m-%d-%H%M%S_$wx$h.png' -e 'mv $f ~/screenshots'
+    fi
 else
     echo "Need a mode"
     print_help
